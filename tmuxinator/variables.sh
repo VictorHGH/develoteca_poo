5f699eee0480ad74c5b6aec56f41b9327840fdfd
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-03-26 15:02:16 -0600

    [Removed] unescesary if statment from variable file

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-# Detect the operating system
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-	link1="dev_poo.test:8080"
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux
-	link1="dev_poo.test:8080"
-else
-    echo "Unsupported operating system"
-    exit 1
-fi
-
+link1="dev_poo.test:8080"
 link2="https://cursos.develoteca.com/courses/take/poo-php-web/lessons/60435878-2-7-manipulacion-de-atributos-en-php"
 
 export link1
