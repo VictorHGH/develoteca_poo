f841e67b9e9edcf01a3dd9d7952e7dfb1e63041f
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-04-02 23:49:43 -0600

    [Finished] Ejercicio 7

---
+++ 007_ejercicio7.php
@@ -0,0 +1,39 @@
+<?php
+
+// Ejemplo de encapsulamiento
+class CuentaBancaria {
+	// Atributos
+	public $saldo;
+	protected $numeroCuenta;
+	private $pin;
+
+	// Métodos
+	public function __construct($saldo, $numeroCuenta, $pin) {
+		$this->saldo = $saldo;
+		$this->numeroCuenta = $numeroCuenta;
+		$this->pin = $pin;
+	}
+
+	public function mostrarSaldo() {
+		return $this->saldo;
+	}
+
+	public function mostrarNumeroCuenta() {
+		return $this->numeroCuenta;
+	}
+
+	public function mostrarPin() {
+		return $this->pin;
+	}
+}
+
+// Instanciación del objeto
+$miCuenta = new CuentaBancaria(100, '12345', 555);
+
+echo 'El saldo es: ' . $miCuenta->mostrarSaldo();
+echo "\n";
+echo 'El numero de cuenta es: ' . $miCuenta->mostrarNumeroCuenta();
+echo "\n";
+echo 'El pin es: ' . $miCuenta->mostrarPin();
+
+?>
