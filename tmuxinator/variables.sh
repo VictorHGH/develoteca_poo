44bc88b7a2e8432c960c80611b9b9a2aee7e3aab
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2025-04-02 23:50:10 -0600

    [Updated] variables

--- tmuxinator/variables.sh
+++ tmuxinator/variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 link1="dev_poo.test:8080"
-link2="https://cursos.develoteca.com/courses/take/poo-php-web/lessons/60435878-2-7-manipulacion-de-atributos-en-php"
+link2="https://cursos.develoteca.com/courses/take/poo-php-web/lessons/60450654-3-5-aplicando-getters-y-setters-clase-login"
 
 export link1
 export link2
